083ace81a0d0b0dedd4c2ad394de2d13bb2b5578
ducthang3202 <ducthangjandl3202@gmail.com>
2025-05-21 12:34:54 +0200

    hello

--- test.java
+++ test.java
@@ -1,26 +1,15 @@
 public class test
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class test
-     */
-    public test()
+    public static void main(String args[])
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        int obGr = 100;
+        int mult = 1;
+        
+        for(int i=1;i<=obGr;i++)
+        {
+            mult += i;            
+        }
+        
+        System.out.println("Mult = " + mult);
     }
 }
